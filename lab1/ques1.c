8f4fb99dfbe9613c644fed5c8af734cfa730d683
Abhishek kumar das <ak2096565@gmail.com>
2026-09-23 22:52:30 +0530

    set up

---
+++ lab1/ques1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter the size of array :");
+    scanf("%d ",&n);
+    int a[n];
+    //printf("enter the value of array");
+    
+    for(int i=0;i<n;i++){
+        printf("Enter array %d ",i);
+        scanf("%d",&a[i]); 
+    }
+    
+    for(int j=n-1;j>=0;j--){
+        printf("%d ",a[j]);
+    }
+}
